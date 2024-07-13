ea15c6983e7e83e9ccc42687ba4c3b97f4ea1ade
Meng Vuthvitou <121754633+Vitoumeng@users.noreply.github.com>
2024-07-13 15:51:45 +0700

    Update index.js - add Scroll active nav__link

--- assets/js/index.js
+++ assets/js/index.js
@@ -68,3 +68,24 @@ const scrollup = () => {
 };
 
 window.addEventListener("scroll", scrollup);
+// Scroll active nav__link
+// Scroll active-link
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active')
+		}else{
+			sectionsClass.classList.remove('active')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
